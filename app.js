4db868ae446a92ce88b4e75f9ea33a7bbdcb2e05
Heshani chamalka <you@example.com>
2021-01-25 18:38:43 +0530

    1st Commit

---
+++ app.js
@@ -0,0 +1,8 @@
+var swap = require("swap-case");
+var camel = require("camel-case");
+
+console.log("Example for Swap Case");
+console.log(swap.swapCase("sWap CaSe"));
+
+console.log("Example for Camel Case");
+console.log(camel.camelCase("Camel Case"));
